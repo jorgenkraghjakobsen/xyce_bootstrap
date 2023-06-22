a923302bff1c2f4820d80b95cd3f3924c7d6893c
Jorgen Kragh Jakobsen <jkj@myrun.dk>
2023-06-22 23:02:29 +0200

    Init commit

---
+++ install_prequired_tools_n_libs.sh
@@ -0,0 +1,29 @@
+#!/bin/sh 
+
+sudo apt-get install -y make cmake bison flex libfl-dev libfftw3-dev libsuitesparse-dev libblas-dev liblapack-dev libtool g++ gfortran autoconf automake git libopenmpi-dev openmpi-bin 
+
+#Get Trilions 12.12.1
+#wget https://github.com/trilinos/Trilinos/archive/refs/tags/trilinos-release-12-12-1.tar.gz
+
+
+tar zxf trilinos-release-12-12-1.tar.gz
+
+cd Trilinos-trilinos-release-12-12-1
+
+cp ../reconfig_trilions .
+
+chmod 755 reconfig_trilions
+
+mkdir build 
+
+cd build 
+
+../reconfig_trilions 
+
+time make -j$(nproc)
+
+make install 
+
+
+  
+
---
+++ readme.md
@@ -0,0 +1,15 @@
+Single command bootstap install xyce simulator on Ubuntu 22.04
+
+=============================================================================
+
+Run 2 scripts 
+
+1: Install requirement libs and tools, including Trilion-12-12-1 (old verison need to be compiled from source)  
+   Require sudo access to install libs and tools and Trilions 
+   Process takes takes around 10 min on 4 core i7 laptop
+   
+   
+2:         
+
+
+ 
