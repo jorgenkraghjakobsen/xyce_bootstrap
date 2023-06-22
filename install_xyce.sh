e9de1b089488726e60f94d98b9240fc8e81537c3
Jorgen Kragh Jakobsen <jkj@myrun.dk>
2023-06-22 23:24:09 +0200

    Added xyce install

---
+++ install_xyce.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#Get Xyce latest release 7.6 
+wget https://www.myrun.dk/src/Xyce-7.6.tar.gz 
+
+tar xcf Xyce-7.6.tar.gz 
+
+cd Xyce-7.6 
+cp ../reconfig_xyce 
+chmod 755 reconfig_xyce
+mkdir build
+cd build
+../reconfig_xyce 
+
+time make -j$(nproc)
+
+make install 
+
+
+
